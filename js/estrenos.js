29c5fe3921350968d94b2ced7d94530fce480dcf
juanchi26 <juanchi.diaz@hotmail.com>
2023-08-27 00:44:00 -0300

    cambios 1.1

--- js/estrenos.js
+++ js/estrenos.js
@@ -36,8 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="col">
             <a class="card mt-3" style="width: 18rem;"">
             <div>
-              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"  onclick="redirect(${movie.id})" alt="Poster de la pelicula">
-              <div class="contenido">
+              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"" alt="Poster de la pelicula">
+              <div onclick="redirect(${movie.id})" class="contenido">
                 <h6>${movie.title}</h6>
                 <p>Puntuacion ${movie.vote_average} <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#e6d837}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></p>
               </div>
@@ -56,8 +56,8 @@ document.addEventListener("DOMContentLoaded", () => {
         <div class="col">
             <a class="card mt-3" style="width: 18rem;"">
             <div>
-              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"  onclick="redirect(${movie.id})" alt="Poster de la pelicula">
-              <div class="contenido">
+              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"" alt="Poster de la pelicula">
+              <div onclick="redirect(${movie.id})" class="contenido">
                 <h6>${movie.title}</h6>
                 <p>Puntuacion ${movie.vote_average} <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#e6d837}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></p>
               </div>
@@ -77,8 +77,8 @@ cargar_mas.addEventListener("click", () => {
           <div class="col">
             <a class="card mt-3" style="width: 18rem;"">
             <div>
-              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"  onclick="redirect(${movie.id})" alt="Poster de la pelicula">
-              <div class="contenido">
+              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top"" alt="Poster de la pelicula">
+              <div onclick="redirect(${movie.id})" class="contenido">
                 <h6>${movie.title}</h6>
                 <p>Puntuacion ${movie.vote_average} <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#e6d837}</style><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></p>
               </div>
